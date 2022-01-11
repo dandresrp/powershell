501db8ab503f9d242c3dc52b3e765a83fdc7714a
Diego Rios <dandresrp00@gmail.com>
2022-01-11 10:12:52 -0600

    Add powershell config

---
+++ user_profile.ps1
@@ -0,0 +1,29 @@
+<# if (!(Get-Module "oh-my-posh")) {
+    Install-Module oh-my-posh -Scope CurrentUser -Force
+}
+
+if (!(Get-Module "posh-git")) {
+    Install-Module -Name posh-git -Scope CurrentUser -Force
+}
+
+if (!(Get-Module "Terminal-Icons")) {
+    Install-Module -Name Terminal-Icons -Scope CurrentUser -Force
+} #>
+
+# Modules
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+
+# Prompt
+$omp_config = Join-Path $PSScriptRoot "\.dandresrp.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+$pwshconfig = Join-Path $HOME "\.config\powershell\user_profile.ps1"
+$pwshpath = Join-Path $HOME "\.config\powershell\"
+
+# Aliases
+#Set-Alias vim nvim
+#Set-Alias ll ls
+#Set-Alias g git
+Set-Alias touch ni
