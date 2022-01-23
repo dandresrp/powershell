5c943345c3632e76d609763f3502e1e00e9b2c69
Diego Rios <dandresrp00@gmail.com>
2022-01-23 15:19:11 -0600

    Replaced omp with starship prompt

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,12 +1,15 @@
 <# Modules #>
-Import-Module posh-git
-Import-Module oh-my-posh
+# Import-Module posh-git
+# Import-Module oh-my-posh
 Import-Module -Name Terminal-Icons
 
 <# Prompt #>
-$omp_config = Join-Path $PSScriptRoot "\.star.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-$env:POSH_GIT_ENABLED = $true
+# $omp_config = Join-Path $PSScriptRoot "\.star.omp.json"
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+# $env:POSH_GIT_ENABLED = $true
+
+$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
+Invoke-Expression (&starship init powershell)
 
 $pwshconfig = Join-Path $HOME "\.config\powershell\user_profile.ps1"
 $pwshpath = Join-Path $HOME "\.config\powershell\"
