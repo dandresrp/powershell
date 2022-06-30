f6f1f87f25c8488740536c20751fc8d0ba7dc7bb
Diego Rios <dandresrp00@gmail.com>
2022-06-30 09:36:19 -0600

    update user_profile

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,11 +1,13 @@
 Import-Module -Name Terminal-Icons
 
-$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
-Invoke-Expression (&starship init powershell)
+#$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
+#Invoke-Expression (&starship init powershell)
 
 $pwshconfig = Join-Path $HOME "\.config\powershell\user_profile.ps1"
 $pwshpath = Join-Path $HOME "\.config\powershell\"
 
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussel.omp.json" | Invoke-Expression
+
 <# Aliases #>
 Set-Alias touch ni
-Set-Alias vim nvim
+Set-Alias notepad++ "C:\Program Files (x86)\Notepad++\notepad++.exe"
