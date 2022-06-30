448d7f436c0002ec0c196a1e219ca0e8883bbe4f
Diego Rios <dandresrp00@gmail.com>
2022-06-30 09:37:01 -0600

    update user_profile

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,8 +1,5 @@
 Import-Module -Name Terminal-Icons
 
-#$ENV:STARSHIP_CONFIG = "$HOME\.config\powershell\starship.toml"
-#Invoke-Expression (&starship init powershell)
-
 $pwshconfig = Join-Path $HOME "\.config\powershell\user_profile.ps1"
 $pwshpath = Join-Path $HOME "\.config\powershell\"
 
